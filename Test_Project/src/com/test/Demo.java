a61305e75f9883d26c446a6b4c477db8681ed20a
Sony <rajeshku1255@gmail.com>
2018-10-21 17:27:37 +0530

    Hello

---
+++ Test_Project/src/com/test/Demo.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Demo {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String s1="Rajesh";
+		String s2="Raj";
+		System.out.println(s1 + s2);
+
+	}
+
+}
